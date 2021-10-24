4bcd25024ca3db07c88c54736d0a0059c6d077c1
mckinseyleckenby <leckenbym11@gmail.com>
2021-10-24 10:25:14 -0500

    Complete

---
+++ index.js
@@ -0,0 +1,21 @@
+const sum = (number1, number2) => {
+    return number1 + number2
+}
+sum(2,3)
+
+const nums = [1,2,3];
+const squares = nums.map(x => x **2);
+squares;
+//=> [1,4,9]
+nums;
+//=> [1,2,3]
+
+// const divide = (number1, number2) => {
+//     return number1 / number2
+// }
+// divide(2000, 100)
+
+const divide = () => 2000/100
+
+const square = x => x * x
+const add = (y, z) => y + z
